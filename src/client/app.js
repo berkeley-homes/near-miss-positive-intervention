839baec963fc8118754560325e76dba13eb3c008
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-20 13:07:05 +0000

    adds first component test
    + using enzyme and tape
    
    related #6

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-class App extends React.Component {
-  render () {
-    return <p>Near Miss - Positive Interventions</p>
-  }
-}
+import Hello from './components/hello.js'
 
-render(<App />, document.getElementById('app'))
+render(<Hello />, document.getElementById('app'))
---
+++ src/client/components/hello.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Hello = () => <p>Near Miss - Positive Interventions</p>
+
+export default Hello
