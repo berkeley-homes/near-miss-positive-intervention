e9f32a9aa0099e18f56ae778a2e41360f00ed764
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-21 14:49:13 +0000

    adds initial db setup and tests
    related #11

---
+++ src/server/db.js
@@ -0,0 +1,43 @@
+const pg = require('pg')
+
+const startConnection = connectionConfig => {
+  const poolConfig = {
+    idleTimeoutMillis: 30000,
+    max: 10
+  }
+  const config = Object.assign({}, poolConfig, connectionConfig)
+
+  const pool = new pg.Pool(config)
+
+  /* istanbul ignore next */
+  pool.on('error', function (err, client) {
+    console.error('idle client error', err.message, err.stack)
+  })
+
+  // wrapper around pg.pool.connect to make query simple
+  return (...queryArgsWithCb) => {
+    const numQueryArgs = queryArgsWithCb.length - 1
+    const cb = queryArgsWithCb[numQueryArgs]
+    const queryArgs = queryArgsWithCb.slice(0, numQueryArgs)
+    pool.connect((connectionError, client, done) => {
+
+      /* istanbul ignore next */
+      if(connectionError) {
+        console.error('error fetching client from pool', connectionError)
+        return cb(connectionError)
+      }
+
+      client.query(...queryArgs, (queryError, result) => {
+        done(queryError)
+
+        if (queryError) return cb(queryError)
+
+        cb(null, result.rows)
+      })
+    })
+  }
+}
+
+module.exports = {
+  startConnection
+}
---
+++ test/server/db.js
@@ -0,0 +1,40 @@
+const test = require('tape')
+
+const db = require('../../src/server/db.js')
+
+const connectionConfig = {
+  user: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 5432,
+  database: process.env.DB_NAME || 'near_miss_test',
+  idleTimeoutMillis: 1 // so test does not hang
+}
+
+test('connect to database', t => {
+  t.doesNotThrow(
+    () => { db.startConnection(connectionConfig) },
+    'does not throw'
+  )
+
+  t.end()
+})
+
+test('run some queries', t => {
+  const query = db.startConnection(connectionConfig)
+
+  t.plan(4)
+  query('nonsence', error => {
+    t.ok(error, 'bad query returns error')
+    t.equal(
+      error.message,
+      'syntax error at or near "nonsence"',
+      'error message is correct'
+    )
+  })
+
+  query('SELECT datname FROM pg_database', (error, result) => {
+    t.error(error, 'good query does not throw error')
+    t.ok(result, 'good query has truthy result')
+  })
+})
