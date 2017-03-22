a3b72c8db5ced5193f846c27455b42ec444a3fef
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-22 08:56:42 +0000

    adds submit report query
    related #11

--- src/server/db.js
+++ src/server/db.js
@@ -57,8 +57,42 @@ const init = (query, cb) => {
   runSqlFromFs(query, 'schema', {}, cb)
 }
 
+const submitReport = (
+  query,
+  {
+    submitterName,
+    locationFirst,
+    locationSecond,
+    locationThird,
+    description,
+    reportType,
+    photoS3Key
+  },
+  cb
+) => {
+  if (!['near miss', 'positive intervention'].includes(reportType)) {
+    return cb(new Error('Report submission failed. Bad report type.'))
+  }
+
+  runSqlFromFs(
+    query,
+    'submit_report',
+    { queryArgs: [
+      submitterName,
+      locationFirst,
+      locationSecond,
+      locationThird,
+      description,
+      reportType,
+      photoS3Key
+    ] },
+    cb
+  )
+}
+
 module.exports = {
   startConnection,
   runSqlFromFs,
-  init
+  init,
+  submitReport
 }
--- test/server/db.js
+++ test/server/db.js
@@ -2,6 +2,20 @@ const test = require('tape')
 
 const db = require('../../src/server/db.js')
 
+const makeMockQuery = () => {
+  const queriesMade = []
+  return {
+    queriesMade,
+    query: (queryStr, queryArgs, cb) => {
+      queriesMade.push({
+        text: queryStr,
+        args: queryArgs
+      })
+      cb()
+    }
+  }
+}
+
 const connectionConfig = {
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
@@ -40,32 +54,81 @@ test('run some queries', t => {
 })
 
 test('runSqlFromFs', t => {
-  const mockQuery = (_, __, cb) => { cb() }
+  const { query } = makeMockQuery()
 
   t.plan(2)
-  db.runSqlFromFs(mockQuery, 'notafile', {}, error => {
+  db.runSqlFromFs(query, 'notafile', {}, error => {
     t.equal(error.message.indexOf('ENOENT'), 0, 'bad path gives ENOENT')
   })
 
-  db.runSqlFromFs(mockQuery, 'schema', {}, error => {
+  db.runSqlFromFs(query, 'schema', {}, error => {
     t.error(error, 'bad path does not give error')
   })
 })
 
 test('build db', t => {
-  const queriesMade = []
-  const mockQuery = (queryStr, _, cb) => {
-    queriesMade.push(queryStr)
-    cb()
-  }
+  const { query, queriesMade } = makeMockQuery()
 
-  db.init(mockQuery, err => {
+  db.init(query, err => {
     t.error(err, 'no error')
 
+    const queryText = queriesMade[0] && queriesMade[0].text
     t.ok(
-      queriesMade[0] &&
-        queriesMade[0].includes('submission_id SERIAL PRIMARY KEY NOT NULL'),
+      queryText &&
+        queryText.includes('submission_id SERIAL PRIMARY KEY NOT NULL'),
       'schema passed to query'
+    )
+    t.end()
+  })
+})
+
+test('submitReport', t => {
+  const { query, queriesMade } = makeMockQuery()
+
+  const submitterName = 'sam'
+  const locationFirst = 'block1'
+  const locationSecond = 'floor2'
+  const locationThird = 'room3'
+  const description = 'car crash'
+  const reportType = 'near miss'
+  const photoS3Key = '123.png'
+
+  db.submitReport(
+    query,
+    {
+      submitterName,
+      locationFirst,
+      locationSecond,
+      locationThird,
+      description,
+      reportType,
+      photoS3Key
+    },
+    () => {
+      const queryMade = queriesMade[0]
+
+      t.equal(
+        queryMade.text.indexOf('INSERT INTO reports'),
+        0,
+        'inserts to correct table'
+      )
+      t.equal(queryMade.args.length, 7, 'with the correct number of arguments')
+      t.end()
+    }
+  )
+})
+
+test('submitReport with bad type', t => {
+  const { query } = makeMockQuery()
+
+  const reportType = 'not a correct type'
+
+  db.submitReport(query, { reportType }, error => {
+    t.ok(error, 'returns error')
+    t.equal(
+      error.message,
+      'Report submission failed. Bad report type.',
+      'inserts to correct table'
     )
     t.end()
   })
