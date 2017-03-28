3944a5f8d44717102bfaaa3072071fc7156ee54e
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-28 10:47:06 +0100

    adds set_posting reducer
    related #25

--- test/client/reducers/upload_photo.js
+++ test/client/reducers/upload_photo.js
@@ -4,9 +4,23 @@ import reducer, { initialState }
   from '../../../src/client/reducers/upload_photo.js'
 import { SET_PHOTO } from '../../../src/client/action_types.js'
 
+test('uploadPhoto: initailState', t => {
+  t.equal(initialState.get('isPosting'), false, 'not posting request initially')
+
+  t.end()
+})
+
 test('uploadPhoto reducer initialState', t => {
   const newState = reducer(undefined, {})
   t.equal(newState, initialState, 'uploadPhoto reducer')
+  t.end()
+})
+
+test('uploadPhoto: SET_POSTING', t => {
+  const newState = reducer(initialState, { type: 'SET_POSTING' })
+
+  t.equal(newState.get('isPosting'), true, 'sets isPosting to true')
+
   t.end()
 })
 
