1f2a87f04bf64b80b2491fe0895935468f1a3e54
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-28 11:26:41 +0100

    sets up redux-thunk
    related #25

--- src/client/store.js
+++ src/client/store.js
@@ -1,11 +1,12 @@
 import { applyMiddleware, createStore } from 'redux'
 import createLogger from 'redux-logger'
+import thunk from 'redux-thunk'
 
 import reducer from './reducers/'
 
 const store = createStore(
    reducer,
-   applyMiddleware(createLogger())
+   applyMiddleware(createLogger(), thunk)
  )
 
 export default store
