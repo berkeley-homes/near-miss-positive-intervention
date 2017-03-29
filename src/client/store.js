05a2e8c495561e0297b085026f97a8a939418cd2
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-29 15:19:14 +0100

    adds state transformer to logger middleware
    related #29

--- src/client/store.js
+++ src/client/store.js
@@ -5,7 +5,7 @@ import reducer from './reducers/'
 
 const store = createStore(
    reducer,
-   applyMiddleware(createLogger())
+   applyMiddleware(createLogger({ stateTransformer: state => state.toJS() }))
  )
 
 export default store
