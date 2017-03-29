6d134349262ae5d7eba886d24e4701330713cfb2
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-29 16:03:48 +0100

    adds location selector to view
    related #29

--- src/client/containers/report_details.js
+++ src/client/containers/report_details.js
@@ -1,15 +1,53 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Immutable from 'immutable'
 
 import * as actions from '../actions/report_details'
+import LocationSelector from '../components/location_selector.js'
+
+const optionsTree = Immutable.fromJS({
+  a: {
+    b: {
+      c: {},
+      d: {},
+      e: {}
+    },
+    f: {
+      g: {},
+      h: {},
+      i: {}
+    },
+    j: {
+      k: {},
+      l: {},
+      m: {}
+    }
+  },
+  n: {
+    o: {
+      p: {},
+      q: {},
+      r: {}
+    },
+    s: {
+      t: {}
+    }
+  }
+})
 
 export const ReportDetails = props => {
   const {
     name,
     description,
     setName,
-    setDescription
+    setDescription,
+    locationSelectorProps
   } = props
+
+  const allLocationSelectorProps = {
+    optionsTree,
+    ...locationSelectorProps
+  }
 
   return (
     <div>
@@ -17,6 +55,7 @@ export const ReportDetails = props => {
         value={name}
         onChange={e => setName(e.target.value)}
       />
+      <LocationSelector {...allLocationSelectorProps} />
       <input
         value={description}
         onChange={e => setDescription(e.target.value)}
@@ -27,7 +66,26 @@ export const ReportDetails = props => {
 
 export const mapStateToProps = state => ({
   name: state.get('name'),
-  description: state.get('description')
+  description: state.get('description'),
+  locationOne: state.getIn(['location', 0]),
+  locationTwo: state.getIn(['location', 1]),
+  locationThree: state.getIn(['location', 2])
 })
 
-export default connect(mapStateToProps, actions)(ReportDetails)
+export const mergeProps = (
+  { locationOne, locationTwo, locationThree, ...stateProps },
+  { setFirstLocation, setSecondLocation, setThirdLocation, ...actionProps }
+) => ({
+  ...stateProps,
+  ...actionProps,
+  locationSelectorProps: {
+    locationOne,
+    locationTwo,
+    locationThree,
+    setFirstLocation,
+    setSecondLocation,
+    setThirdLocation
+  }
+})
+
+export default connect(mapStateToProps, actions, mergeProps)(ReportDetails)
--- test/client/containers/report_details.js
+++ test/client/containers/report_details.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme'
 import React from 'react'
 import Immutable from 'immutable'
 
-import { ReportDetails, mapStateToProps }
+import { ReportDetails, mapStateToProps, mergeProps }
   from '../../../src/client/containers/report_details.js'
 
 test('<ReportDetails /> component', t => {
@@ -44,13 +44,47 @@ test('<ReportDetails /> component', t => {
 test('ReportDetails mapStateToProps', t => {
   const name = 'name'
   const description = 'description'
-  const state = Immutable.fromJS({ name, description })
+  const locationOne = 'location one'
+  const locationTwo = 'location two'
+  const locationThree = 'location three'
+  const location = [locationOne, locationTwo, locationThree]
+
+  const state = Immutable.fromJS({ name, description, location })
   const props = mapStateToProps(state)
 
+  t.equal(props.name, name, 'name pulled from state')
+  t.equal(props.description, description, 'description pulled from state')
+  t.equal(props.locationOne, locationOne, 'location 1 pulled from state')
+  t.equal(props.locationTwo, locationTwo, 'location 2 pulled from state')
+  t.equal(props.locationThree, locationThree, 'location 3 pulled from state')
+
+  t.end()
+})
+
+const filterUnderfined = obj => JSON.parse(JSON.stringify(obj))
+
+test('ReportDetails mergeProps', t => {
+  const description = 'description'
+  const locationOne = 'location one'
+
+  const setName = 'set name action'
+  const setFirstLocation = 'set first location action'
+
+  const props = mergeProps(
+    { description, locationOne },
+    { setName, setFirstLocation }
+  )
+  const expectedProps = {
+    description,
+    setName,
+    locationSelectorProps: { locationOne, setFirstLocation }
+  }
+
+  // not checking everything as that would be long
   t.deepEqual(
-    props,
-    { name, description },
-    'name and description pulled from state'
+    filterUnderfined(props),
+    expectedProps,
+    'location props get pulled into their own object'
   )
 
   t.end()
