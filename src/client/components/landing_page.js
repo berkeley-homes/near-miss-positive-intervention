f667917c3822d4e8cb0e25a76dd9070783bbdff0
Eoin McCarthy <em1081@my.bristol.ac.uk>
2017-04-04 08:03:46 +0000

    adds link to report page
    related #31

--- src/client/components/landing_page.js
+++ src/client/components/landing_page.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Logo from '../components/logo.js'
 import WelcomeMessage from '../components/welcome_message.js'
+import { Link } from 'react-router-dom'
 
 const LandingPage = () => {
   return (
     <div>
       <Logo />
       <WelcomeMessage />
+      <Link to='/report'> report </Link>
     </div>
   )
 }
