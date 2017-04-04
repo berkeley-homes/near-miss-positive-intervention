bc6c20795e86d1086d29a289b673d774cf95dd66
Esraa <jbesraa@gmail.com>
2017-04-04 17:06:05 +0300

    related to #58 add home button to success page

--- src/client/components/home_button.js
+++ src/client/components/home_button.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 class HomeBtn extends Component {
   render () {
     return (
-      <div className='w-100 g-bg'>
-        <div className='g-bg' />
+      <div className='w-100 g-bg h5'>
+        <button className="bg-white button">BACK TO HOME</button>
       </div>
     )
   }
