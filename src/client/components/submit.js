b86b3eb5bf3f4b7ee74aaa4cb56afaf9f2577e21
Eoin McCarthy <em1081@my.bristol.ac.uk>
2017-04-04 11:28:12 +0000

    fix server endpoint and submission code
    related #45

--- src/client/components/submit.js
+++ src/client/components/submit.js
@@ -2,13 +2,12 @@ import React from 'react'
 import Loading from './upload_animation.js'
 
 export default ({ enabled, submit, isSubmitting }) =>
-  { isSubmitting
+  isSubmitting
     ? <Loading />
     : <img
-          className='center db'
-          src={enabled
-            ? '/assets/send_icon_btn_enabled.svg'
-            : '/assets/send_icon_btn_disabled.svg'}
-          onClick={enabled && submit}
-       />
- }
+        className='center db'
+        src={enabled
+          ? '/assets/send_icon_btn_enabled.svg'
+          : '/assets/send_icon_btn_disabled.svg'}
+        onClick={enabled && submit}
+      />
