1144aac7fcb2c98a996e60f7c8a123902a55bee0
Eoin McCarthy <em1081@my.bristol.ac.uk>
2017-04-04 13:23:09 +0000

    adds routing to thumbs
    related something ??

--- src/client/action_types.js
+++ src/client/action_types.js
@@ -2,6 +2,7 @@ export const SET_PHOTO = 'SET_PHOTO'
 export const SET_NAME = 'SET_NAME'
 export const SET_LOCATION = 'SET_LOCATION'
 export const SET_DESCRIPTION = 'SET_DESCRIPTION'
+export const SET_REPORT_TYPE = 'SET_REPORT_TYPE'
 
 export const SET_POSTING = 'SET_POSTING'
 export const SET_POST_RESULT = 'SET_POST_RESULT'
--- src/client/containers/thumbs.js
+++ src/client/containers/thumbs.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+
+import * as actions from '../actions/thumbs.js'
 
 import ThumbsUp from '../components/thumbs_up.js'
 import ThumbsDown from '../components/thumbs_down.js'
 
-class Thumbs extends Component {
-  render () {
-    return (
-      <div className='container h-100 w-100'>
-        <ThumbsDown />
-        <ThumbsUp />
-      </div>
-    )
-  }
-}
-export default Thumbs
+export const Thumbs = ({ selectNearMiss, selectPositiveIntervention }) =>
+  <div className='container h-100 w-100'>
+    <ThumbsDown onClick = { selectPositiveIntervention }/>
+    <ThumbsUp onClick = { selectNearMiss }/>
+  </div>
+
+export default connect(() => ({}), actions)(Thumbs)
