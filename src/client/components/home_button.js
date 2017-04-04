365f303a23d1a98a2c2c4967da10d4f75dd5dc6b
Esraa <jbesraa@gmail.com>
2017-04-04 17:07:51 +0300

    fix double quotes

--- src/client/components/home_button.js
+++ src/client/components/home_button.js
@@ -4,7 +4,7 @@ class HomeBtn extends Component {
   render () {
     return (
       <div className='w-100 g-bg h5'>
-        <button className="bg-white button">BACK TO HOME</button>
+        <button className='bg-white button'>BACK TO HOME</button>
       </div>
     )
   }
