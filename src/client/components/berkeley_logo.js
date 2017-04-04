d92033081212a2dfce647c39d905b91b09ece148
Esraa <jbesraa@gmail.com>
2017-04-04 16:28:30 +0300

    related to #54 success view

---
+++ src/client/components/berkeley_logo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BerkeleyLogo extends Component {
+  render () {
+    return (
+      <div className='logo-container'>
+        <img className='flex-item height20 mt5' src='/img/Berkeley-Group-logo.png' alt='myimage' />
+      </div>
+    )
+  }
+}
+
+export default BerkeleyLogo
