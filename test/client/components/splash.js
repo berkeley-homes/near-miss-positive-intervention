01079e70a3083936333453e2dbfe115e78ab2edf
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-04-05 15:20:57 +0100

    adds splash screen
    related #62

---
+++ test/client/components/splash.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import test from 'tape'
+
+import Splash from '../../../src/client/components/splash.js'
+
+test('<Splash />', t => {
+  const wrapper = shallow(<Splash />)
+
+  const images = wrapper.find('img')
+
+  t.equal(images.length, 2, 'two images')
+  t.ok(
+    images.at(0).node.props.src.includes('onboarding_logo'),
+    'first img is onboarding logo'
+  )
+  // console.log();
+  t.ok(
+    images.at(1).node.props.src.includes('Berkeley-Group-logo'),
+    'second image is logo'
+  )
+
+  t.end()
+})
