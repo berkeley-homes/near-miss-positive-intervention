e7381da8e87ad66d1291b5eefe48d64ef5cb21f2
Esraa <jbesraa@gmail.com>
2017-04-05 15:03:57 +0300

    test view

--- src/client/components/thumbs_up.js
+++ src/client/components/thumbs_up.js
@@ -4,7 +4,7 @@ class PositiveBtn extends Component {
   render () {
     const { onClick } = this.props
     return (
-      <div onClick={onClick} className='g-bg pl3 pa4 w-100 h-100'>
+      <div onClick={onClick} className='g-bg w-100 h-100'>
         <img className='w-55' src='/img/Thumbs_up_btn_white.svg' alt='myimage' />
         <p className='mt0 white'>Positive Intervention</p>
       </div>
