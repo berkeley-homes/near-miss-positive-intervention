3313d0354e841f72654023aff3acb3f525b8e9e2
Esraa <jbesraa@gmail.com>
2017-04-05 16:05:32 +0300

    test view

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 class Header extends Component {
   render () {
     return (
-      <div className='bb w-100'>
+      <div className='bb w-100 pa1'>
         <div className='height50'>
-          <div className='fl w-30  height50 width50'>
+          <div className='fl w-25  height50 pl1'>
             <img  className="height50" src='/img/NearMiss_nav_logo.svg' alt='berlogo' />
           </div>
-          <div className='fl w-30 pl4 pr2'>
-            <p>HOME</p>
+          <div className='fl w-10 pl5'>
+            <p className='mt3'>HOME</p>
             </div>
-            <div className='fl w-30 height50 mt2 pl4'>
+            <div className='fl w-10 height50 mt3 ml6'>
               <img src='/img/Burger_menu.svg' alt='hammenu' />
             </div>
           </div>
