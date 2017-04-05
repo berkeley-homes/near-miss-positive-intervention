50992ef80b8af9157e4a27aa4c77deca52880b73
Esraa <jbesraa@gmail.com>
2017-04-05 16:55:09 +0300

    view test

--- src/client/components/thumbs_down.js
+++ src/client/components/thumbs_down.js
@@ -4,7 +4,7 @@ class NearMissBtn extends Component {
   render () {
     const { onClick } = this.props
     return (
-      <div onClick={onClick} className='w-100 h-100 thumbs-down mt4 pa3'>
+      <div onClick={onClick} className='w-100 h-100 thumbs-down mt4 pa5'>
         <p className='mb0 tc'>Near Miss</p>
         <img className='w-55'src='/img/Thumbs_down_btn_black.svg' alt='myimage' />
       </div>
--- src/client/components/thumbs_up.js
+++ src/client/components/thumbs_up.js
@@ -4,7 +4,7 @@ class PositiveBtn extends Component {
   render () {
     const { onClick } = this.props
     return (
-      <div onClick={onClick} className='g-bg w-100 pa3'>
+      <div onClick={onClick} className='g-bg w-100 pa5'>
         <img className='w-55 mt3 h-100' src='/img/Thumbs_up_btn_white.svg' alt='myimage' />
         <p className='mt0 white tc'>Positive Intervention</p>
       </div>
