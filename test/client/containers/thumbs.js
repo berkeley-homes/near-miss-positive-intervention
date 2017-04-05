576bf86aa28b0f13905d652cb613c738ec141efe
Eoin McCarthy <em1081@my.bristol.ac.uk>
2017-04-05 09:57:28 +0000

    fix broken import on thumbs container test
      + also fixes bad naming on other thumbs component tests
    
    related #68

--- test/client/containers/thumbs.js
+++ test/client/containers/thumbs.js
@@ -1,11 +1,11 @@
 import test from 'tape'
 import React from 'react'
 import { shallow } from 'enzyme'
-import Thumbs from '../../../src/client/containers/thumbs.js'
+import { Thumbs } from '../../../src/client/containers/thumbs.js'
 import ThumbsUp from '../../../src/client/components/thumbs_up.js'
 import ThumbsDown from '../../../src/client/components/thumbs_down.js'
 
-test('Name input component includes input', t => {
+test('<Thumbs />', t => {
   const wrapper = shallow(<Thumbs />)
   t.equal(wrapper.contains(
     <div className='container h-100 w-100'>
