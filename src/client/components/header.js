e09ce39fa8455fd6846e981d64c56253dcfc949b
Esraa <jbesraa@gmail.com>
2017-04-05 15:57:21 +0300

    test views

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -4,14 +4,14 @@ class Header extends Component {
   render () {
     return (
       <div className='bb w-100'>
-        <div className='height50 ml3'>
-          <div className='fl w-25  height50 width50 pl1'>
+        <div className='height50'>
+          <div className='fl w-30  height50 width50'>
             <img  className="height50" src='/img/NearMiss_nav_logo.svg' alt='berlogo' />
           </div>
-          <div className='fl w-25 pl5'>
+          <div className='fl w-30 pl4 pr2'>
             <p>HOME</p>
             </div>
-            <div className='fl w-30 height50 mt2 ml5'>
+            <div className='fl w-30 height50 mt2 pl4'>
               <img src='/img/Burger_menu.svg' alt='hammenu' />
             </div>
           </div>
