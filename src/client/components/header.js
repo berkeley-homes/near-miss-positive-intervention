5a4dc49794b0ef9313b8ec25f8d02a88d65d68d5
Esraa <jbesraa@gmail.com>
2017-04-12 09:47:37 +0300

    change header to stateless component

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Header extends Component {
-  render () {
-    return (
-      <div className='bb w-100 h3'>
-        <img
-          className='h3 fl w-third'
-          src='/img/nearMiss_nav_logo.svg'
-          alt='NearMissLogo'
+const Header = () => {
+  return (
+    <div className='bb w-100 h3'>
+      <img
+        className='h3 fl w-third'
+        src='/img/nearMiss_nav_logo.svg'
+        alt='NearMissLogo'
         />
-        <p className='fl w-third pl1p5 pr1p5 pt2'>HOME</p>
-        <img
-          className='h2 fl w-third pt3'
-          src='/img/burger_menu.svg'
-          alt='HamburgerMenu'
+      <p className='fl w-third pl1p5 pr1p5 pt2'>HOME</p>
+      <img
+        className='h2 fl w-third pt3'
+        src='/img/burger_menu.svg'
+        alt='HamburgerMenu'
         />
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Header
