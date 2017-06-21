756f7a5ef131ecb25297535408abb2f97fe67848
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-06-21 16:11:14 +0100

    changes thankyou message
    related #98

--- src/client/components/thanks_message.js
+++ src/client/components/thanks_message.js
@@ -4,8 +4,8 @@ class ThanksMessage extends Component {
   render () {
     return (
       <div className='f_francois tc main_black_font f4'>
-        <p className='mb0'>Thanx Pal,</p>
-        <p className='mt0 lh-copy'>We have recieved your email!</p>
+        <p className='mb0'>Thank you,</p>
+        <p className='mt0 lh-copy pa4'>your Near miss or Positive intervention has been recorded.</p>
       </div>
     )
   }
