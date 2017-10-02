c0f4947c5a82d35ac9368b52cd4125b86af1f43b
Sohil Pandya <sohilpandya@Sohils-MacBook-Pro.local>
2017-10-02 12:15:55 +0100

    added server routes for frontend endpoints which were returning 404 related to #105

--- test/server/routes.js
+++ test/server/routes.js
@@ -2,8 +2,32 @@ const test = require('tape')
 
 const server = require('../../src/server/server.js')
 
-test('can retrive index html', t => {
+test('can retrieve index html', t => {
   server.inject({ method: 'get', url: '/' }, response => {
+    t.equal(response.statusCode, 200, 'receive 200')
+    const title = 'Near Miss - Positive Interventions'
+    t.ok(response.payload.includes(title), 'payload includes title')
+    t.end()
+  })
+})
+test('can retrieve index html', t => {
+  server.inject({ method: 'get', url: '/thumbs' }, response => {
+    t.equal(response.statusCode, 200, 'receive 200')
+    const title = 'Near Miss - Positive Interventions'
+    t.ok(response.payload.includes(title), 'payload includes title')
+    t.end()
+  })
+})
+test('can retrieve index html', t => {
+  server.inject({ method: 'get', url: '/report' }, response => {
+    t.equal(response.statusCode, 200, 'receive 200')
+    const title = 'Near Miss - Positive Interventions'
+    t.ok(response.payload.includes(title), 'payload includes title')
+    t.end()
+  })
+})
+test('can retrieve index html', t => {
+  server.inject({ method: 'get', url: '/success' }, response => {
     t.equal(response.statusCode, 200, 'receive 200')
     const title = 'Near Miss - Positive Interventions'
     t.ok(response.payload.includes(title), 'payload includes title')
