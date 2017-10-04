8f948d5da3e29e8f8dd5cc660f24abd09463a603
emily <limeyb7@gmail.com>
2017-10-04 15:42:37 +0100

    add prettier-standard option fix linting errors

--- src/client/components/input.js
+++ src/client/components/input.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default ({ label, value, onChange }) =>
   <div className='w-100 pa3'>
-    <span className='black-60'>{ label }</span>
+    <span className='black-60'>
+      {label}
+    </span>
     <input
       type='text'
       className='w-100 h3 ba bw1 mt2 b--nearmiss-black nearmiss_input_text'
