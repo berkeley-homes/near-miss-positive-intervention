bea65eeb43cab16aff3137ee550d3954434dac1f
emily <limeyb7@gmail.com>
2017-10-05 11:04:26 +0100

    fix styling on thumbs page

--- src/client/components/thumbs_up.js
+++ src/client/components/thumbs_up.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ThumbsUp extends Component {
-  render () {
-    const { onClick } = this.props
+  render() {
+    const { onClick } = this.props;
     return (
-      <div onClick={onClick} className='bg-dark-gray w-100 pv5'>
+      <div
+        onClick={onClick}
+        className="bg-dark-gray w-100 grow-1 flex flex-column justify-center"
+      >
         <img
-          className='center db w-65 h-100'
-          src='/img/Thumbs_up_btn_white.svg'
-          alt='ThumbsUpImg'
+          className="center db h4"
+          src="/img/Thumbs_up_btn_white.svg"
+          alt="ThumbsUpImg"
         />
-        <p className='mt0 white tc'>Positive Intervention</p>
+        <p className="mt0 white tc">Positive Intervention</p>
       </div>
-    )
+    );
   }
 }
 
-export default ThumbsUp
+export default ThumbsUp;
--- src/client/containers/thumbs.js
+++ src/client/containers/thumbs.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-import * as actions from '../actions/thumbs.js'
+import * as actions from "../actions/thumbs.js";
 
-import ThumbsUp from '../components/thumbs_up.js'
-import ThumbsDown from '../components/thumbs_down.js'
-import Header from '../components/header.js'
+import ThumbsUp from "../components/thumbs_up.js";
+import ThumbsDown from "../components/thumbs_down.js";
+import Header from "../components/header.js";
 
-export const Thumbs = ({ selectNearMiss, selectPositiveIntervention }) =>
-  <div className='h-100 w-100 f_lato f4'>
-    <Header location={'HOME'} />
-    <ThumbsDown onClick={selectNearMiss} />
-    <ThumbsUp onClick={selectPositiveIntervention} />
+export const Thumbs = ({ selectNearMiss, selectPositiveIntervention }) => (
+  <div className="vh-100 w-100 f_lato f4 flex flex-column">
+    <Header location={"HOME"} />
+    <div className="flex flex-column h-100">
+      <ThumbsDown onClick={selectNearMiss} />
+      <ThumbsUp onClick={selectPositiveIntervention} />
+    </div>
   </div>
+);
 
-export default connect(() => ({}), actions)(Thumbs)
+export default connect(() => ({}), actions)(Thumbs);
