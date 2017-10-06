f8ce258598ebf4b52522a3f94b4700bc3a335bdd
emily <limeyb7@gmail.com>
2017-10-06 15:34:22 +0100

    fix linting errors

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,33 +1,33 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Header extends Component {
-  render() {
+  render () {
     return (
-      <header className="bb w-100 h3">
-        <div className="fl w-third h3 db">
-          <Link to="/">
+      <header className='bb w-100 h3'>
+        <div className='fl w-third h3 db'>
+          <Link to='/'>
             <img
-              className="h3 fl pointer"
-              src="/img/nearMiss_nav_logo.svg"
-              alt="NearMissLogo"
+              className='h3 fl pointer'
+              src='/img/nearMiss_nav_logo.svg'
+              alt='NearMissLogo'
             />
           </Link>
         </div>
-        <div className="fl w-third pt2 center db">
-          <p className="tc">{this.props.location}</p>
+        <div className='fl w-third pt2 center db'>
+          <p className='tc'>{this.props.location}</p>
         </div>
         {/* hamburger menu commented out because it has no functionality */}
-        <div className="w-third db">
+        <div className='w-third db'>
           <img
-            className="h2 fl o-0"
-            src="/img/burger_menu.svg"
-            alt="HamburgerMenu"
+            className='h2 fl o-0'
+            src='/img/burger_menu.svg'
+            alt='HamburgerMenu'
           />
         </div>
       </header>
-    );
+    )
   }
 }
 
-export default Header;
+export default Header
--- src/client/components/thumbs_down.js
+++ src/client/components/thumbs_down.js
@@ -1,22 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class ThumbsDown extends Component {
-  render() {
-    const { onClick } = this.props;
+  render () {
+    const { onClick } = this.props
     return (
       <div
         onClick={onClick}
-        className="w-100 grow-1 flex flex-column justify-center pointer"
+        className='w-100 grow-1 flex flex-column justify-center pointer'
       >
-        <p className="mb0 tc">Near Miss</p>
+        <p className='mb0 tc'>Near Miss</p>
         <img
-          className="center db h4"
-          src="/img/Thumbs_down_btn_black.svg"
-          alt="ThumbsDownImg"
+          className='center db h4'
+          src='/img/Thumbs_down_btn_black.svg'
+          alt='ThumbsDownImg'
         />
       </div>
-    );
+    )
   }
 }
 
-export default ThumbsDown;
+export default ThumbsDown
