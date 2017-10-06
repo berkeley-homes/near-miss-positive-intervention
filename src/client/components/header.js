fcd77bcdd016bb10841845d1709ff2629a0e6404
emily <limeyb7@gmail.com>
2017-10-06 15:32:22 +0100

    change header logo to link to splash page

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
     return (
       <header className="bb w-100 h3">
         <div className="fl w-third h3 db">
-          <img
-            className="h3 fl"
-            src="/img/nearMiss_nav_logo.svg"
-            alt="NearMissLogo"
-          />
+          <Link to="/">
+            <img
+              className="h3 fl pointer"
+              src="/img/nearMiss_nav_logo.svg"
+              alt="NearMissLogo"
+            />
+          </Link>
         </div>
         <div className="fl w-third pt2 center db">
           <p className="tc">{this.props.location}</p>
