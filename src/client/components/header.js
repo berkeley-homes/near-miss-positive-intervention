b0236f1f92eb844649aec7435cc21586fe6b2942
emily <limeyb7@gmail.com>
2017-10-06 15:28:36 +0100

    fix layout of header

--- src/client/components/header.js
+++ src/client/components/header.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Header extends Component {
-  render () {
+  render() {
     return (
-      <div className='bb w-100 h3'>
-        <img
-          className='h3 fl w-third'
-          src='/img/nearMiss_nav_logo.svg'
-          alt='NearMissLogo'
-        />
-        <div className='fl w-third pt2 center db'>
-          <p className='tc'>
-            {this.props.location}
-          </p>
+      <header className="bb w-100 h3">
+        <div className="fl w-third h3 db">
+          <img
+            className="h3 fl"
+            src="/img/nearMiss_nav_logo.svg"
+            alt="NearMissLogo"
+          />
         </div>
-        <img
-          className='h2 fl w-third pt3'
-          src='/img/burger_menu.svg'
-          alt='HamburgerMenu'
-        />
-      </div>
-    )
+        <div className="fl w-third pt2 center db">
+          <p className="tc">{this.props.location}</p>
+        </div>
+        {/* hamburger menu commented out because it has no functionality */}
+        <div className="w-third db">
+          <img
+            className="h2 fl o-0"
+            src="/img/burger_menu.svg"
+            alt="HamburgerMenu"
+          />
+        </div>
+      </header>
+    );
   }
 }
 
-export default Header
+export default Header;
--- test/client/components/header.js
+++ test/client/components/header.js
@@ -1,21 +1,10 @@
-import test from 'tape'
-import React from 'react'
-import { shallow } from 'enzyme'
-import Header from '../../../src/client/components/header.js'
+import test from "tape";
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "../../../src/client/components/header.js";
 
-test('Header includes NM Logo, title and menubtn', t => {
-  const wrapper = shallow(<Header />)
-  t.equal(wrapper.contains(
-    <img
-      className='h3 fl w-third'
-      src='/img/nearMiss_nav_logo.svg'
-      alt='NearMissLogo'
-  />), true)
-  t.equal(wrapper.contains(
-    <img
-      className='h2 fl w-third pt3'
-      src='/img/burger_menu.svg'
-      alt='HamburgerMenu'
-  />), true)
-  t.end()
-})
+test("Header includes NM Logo, title and menubtn", t => {
+  const wrapper = shallow(<Header />);
+  t.ok(wrapper.find("div").length === 3, "header contains 3 child elements");
+  t.end();
+});
