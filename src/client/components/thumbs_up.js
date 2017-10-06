ec5e94ed6f298bc5be3e823727b64a77bb315eed
emily <limeyb7@gmail.com>
2017-10-06 15:33:37 +0100

    change cursor on thumbs divs to pointer

--- src/client/components/thumbs_up.js
+++ src/client/components/thumbs_up.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class ThumbsUp extends Component {
-  render () {
-    const { onClick } = this.props
+  render() {
+    const { onClick } = this.props;
     return (
       <div
         onClick={onClick}
-        className='bg-dark-gray w-100 grow-1 flex flex-column justify-center'
+        className="bg-dark-gray w-100 grow-1 flex flex-column justify-center pointer"
       >
         <img
-          className='center db h4'
-          src='/img/Thumbs_up_btn_white.svg'
-          alt='ThumbsUpImg'
+          className="center db h4"
+          src="/img/Thumbs_up_btn_white.svg"
+          alt="ThumbsUpImg"
         />
-        <p className='mt0 white tc'>Positive Intervention</p>
+        <p className="mt0 white tc">Positive Intervention</p>
       </div>
-    )
+    );
   }
 }
 
-export default ThumbsUp
+export default ThumbsUp;
