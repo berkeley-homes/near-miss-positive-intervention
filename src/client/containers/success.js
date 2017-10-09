188a856a4e39c917673a887c7b4c3b8da6af8bdb
emily <limeyb7@gmail.com>
2017-10-09 16:53:05 +0100

    add message to user when we get 500 error related #117

--- src/client/containers/success.js
+++ src/client/containers/success.js
@@ -1,23 +1,42 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import immutable from "immutable";
 
-import BerkeleyLogo from '../components/berkeley_logo.js'
-import ThanksMessage from '../components/thanks_message.js'
-import SuccessIllustration from '../components/success_illustration.js'
-import HomeBtn from '../components/home_button.js'
-import Header from '../components/header.js'
+import BerkeleyLogo from "../components/berkeley_logo.js";
+import ThanksMessage from "../components/thanks_message.js";
+import SuccessIllustration from "../components/success_illustration.js";
+import HomeBtn from "../components/home_button.js";
+import Header from "../components/header.js";
 
 class Success extends Component {
-  render () {
+  render() {
+    const { payload } = this.props;
     return (
-      <div>
-        <Header location={'SUCCESS'} />
+      <div className="f_lato">
+        <Header location={"SUCCESS"} />
         <BerkeleyLogo />
-        <ThanksMessage />
-        <SuccessIllustration />
-        <Link to='/thumbs'><HomeBtn /></Link>
+        {payload.status === 200 ? (
+          <div>
+            <ThanksMessage />
+            <SuccessIllustration />
+          </div>
+        ) : (
+          <div>
+            <p>
+              Hey there! Something's gone wrong. Please try again by clicking
+              the link below. If you continue to have problems, please get in
+              touch.
+            </p>
+          </div>
+        )}
+        <Link to="/thumbs">
+          <HomeBtn />
+        </Link>
       </div>
-    )
+    );
   }
 }
-export default Success
+export default connect(state => ({
+  payload: immutable.fromJS(state.report.get("payload"))
+}))(Success);
